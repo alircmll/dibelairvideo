45d71543bca962f18acec9f6be3c4ae454b9cdfa
rcmll <alice_rc@live.fr>
2020-06-13 21:05:36 -0400

    front nav + start footer

--- app/javascript/plugins/solidnav.js
+++ app/javascript/plugins/solidnav.js
@@ -1,6 +1,6 @@
 jQuery(function($) {
   $(window).scroll(function(){
-      if($(window).scrollTop() > 10) {
+      if($(window).scrollTop() > 200) {
             $('.navbar').addClass("solid-nav");
       } else {
             $('.navbar').removeClass("solid-nav");
