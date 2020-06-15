c95e7e79daaab5a38e624c3f6184e77135edd381
rcmll <alice_rc@live.fr>
2020-06-15 17:03:44 -0400

    js folio

--- app/javascript/plugins/folio.js
+++ app/javascript/plugins/folio.js
@@ -1,9 +1,23 @@
+
 const folio = () =>{
   $(".option").click(function(){
      $(".option").removeClass("active");
      $(this).addClass("active");
 
+    // si option n'a pas de class avtive
+    // alors mettre la classe active sur le 1er
   });
 };
 
+
+let options = document.querySelectorAll(".option")
+
+options.forEach((opt, index)=>{
+  if (! (opt).classList.contains("active") && index === 3){
+    opt.classList.add("active");
+  };
+});
+
 export { folio };
+
+
